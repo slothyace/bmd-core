77733a7bb5c618b5152976031a06e2bf6a4d2328
Acedia <110048632+slothyace@users.noreply.github.com>
2024-11-19 18:28:57 +0800

    Delete Events directory

--- Events/bulk_delete_messages.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "Messages Bulk Delete",
-  nameSchemes: ["Store Deleted Messages List As"],
-  initialize(client, data, run) {
-    client.on('messageDeleteBulk', (messages) => {
-      run(messages, messages[0])
-    })
-  }
-};
